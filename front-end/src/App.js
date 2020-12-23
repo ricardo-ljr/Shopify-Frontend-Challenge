d877967ddb4e0199b721091c9f219afaead9ff90
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-12-22 19:04:00 -0700

    Completed Search Query Process

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Searchbar from "./components/SearchBar";
+function App() {
+  return (
+    <div className="App">
+      <Searchbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/SearchBar.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./components.css";
+
+const SearchBar = () => {
+  const [data, setData] = useState("");
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const onInputChange = e => {
+    setQuery(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const fetchData = e => {
+    e.preventDefault();
+    axios
+      .get(`http://www.omdbapi.com/?s=${query}&type=movie&apikey=${API_KEY}`)
+      .then(res => {
+        console.log(res.data);
+        setData({});
+        setLoading(true);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div>
+      <div className="form-container">
+        <form className="form-inline mt-4 mb-4" onSubmit={fetchData}>
+          <i className="fa fa-search search-icon" />
+          <input
+            className="form-control form-control-sm ml-3 w-75"
+            type="text"
+            placeholder="Search..."
+            aria-label="Search..."
+            onChange={onInputChange}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
