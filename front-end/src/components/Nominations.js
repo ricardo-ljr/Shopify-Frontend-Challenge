90420c9dfb516b88e05716a1b4ba5a16832e9694
Ricardo Leite <ricardo.leitejr95@gmail.com>
2021-01-04 07:04:58 -0700

    Added simple nominations component

--- front-end/src/components/Nominations.js
+++ front-end/src/components/Nominations.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { monitorEventLoopDelay } from "perf_hooks";
 
-const Nominations = () => {
-  return <div></div>;
+const Nominations = props => {
+  return (
+    <div>
+      {props.nominees.map((nominee, index) => {
+        return (
+          <div>
+            <ul key={index}></ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Nominations;
